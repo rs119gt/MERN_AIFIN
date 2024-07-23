0781875e066162dd0af73cd731e18f0ffef06de8
rs119gt <ricksen119@gmail.com>
2024-07-23 19:08:57 +0530

    router

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css'
 import Add from './components/add'
-
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom'
 
 function App() {
 return(
-  <div>Home</div>
- /* <Add /> */
+  <>
+
+ <Router>
+  <Routes>
+  <Route path="/" element={<Add/>}/>
+  <Route path="/shop" element={<h1>Shop</h1>}/>
+  </Routes>
+ </Router>
+ </>
 )
 }
 
