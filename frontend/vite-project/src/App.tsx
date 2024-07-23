a70b1324ba4677eb3ffe51d67db067fd165f0f77
rs119gt <ricksen119@gmail.com>
2024-07-23 20:03:46 +0530

    authentication

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Add from './components/add'
 import { BrowserRouter as Router, Route,Routes } from 'react-router-dom'
+import Dashboard from './pages/dashboard'
+import Auth from './pages/auth'
 
 function App() {
 return(
@@ -8,7 +10,9 @@ return(
 
  <Router>
   <Routes>
-  <Route path="/" element={<Add/>}/>
+  <Route path="/" element={<Dashboard/>}/>
+  <Route path="/signin" element={<Auth />}/>
+  <Route path="/users" element={<Add/>}/>
   <Route path="/shop" element={<h1>Shop</h1>}/>
   </Routes>
  </Router>
